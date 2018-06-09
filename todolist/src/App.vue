175130bdb1f426f44f77f14d1b2d80af0b9039ab
郭小琛 <guoxiaochen8@163.com>
2018-06-09 12:30:11 +0800

    在vue-cli项目里实现todolist

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <input v-model="inputValue"/>
+    <button @click="handleSubmit">提交</button>
+    <ul>
+      <todo-item v-for="(item,index) of list"
+        :key="index"
+        :content="item"
+        :index="index"
+        @delete="handleDelete"
+      ></todo-item>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TodoItem from './components/TodoItem'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components : {
+    'todo-item':TodoItem
+  },
+  data () {
+    return {
+      inputValue : '',
+      list : []
+    }
+  },
+  methods : {
+    handleSubmit () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete (index) {
+      this.list.splice(index , 1)
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
---
+++ todolist/src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li @click="handleClick">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props:['content' , 'index'],
+  methods:{
+    handleClick () {
+      this.$emit('delete' , this.index)
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
